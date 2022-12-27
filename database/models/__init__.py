a5e2fc8f80a28125ce61fe5e58f72d0ebe629f54
ARubioS <alvaro.rubio.segovia@gmail.com>
2022-12-27 12:15:40 +0100

    Make Base more robust

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -1,9 +1,30 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import declarative_base
-Base = declarative_base()
 
-def import_models():
-    from .user import User
-    from .address import Address
-    from .role import Role
+# Metadata for ORM: https://docs.sqlalchemy.org/en/14/core/metadata.html
+# Naming convention: https://alembic.sqlalchemy.org/en/latest/naming.html
+meta: MetaData = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    })
+
+# Base for ORM: https://docs.sqlalchemy.org/en/14/orm/mapping_api.html#sqlalchemy.orm.declarative_base
+Base = declarative_base(metadata=meta)
+
+# TODO: Set up a logger for this
+def import_models() -> None:
+    """Import models to be used in database by Base"""
+    try:
+        from .user import User
+        from .address import Address
+        from .role import Role
+    except ImportError as e:
+        print(f"Import error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
 import_models()
